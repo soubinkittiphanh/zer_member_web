e3d7a6d529b558f3eabc2d3a9eda3549ff91a777
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2023-03-21 15:58:16 +0700

    change app title

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,8 +3,8 @@ import colors from 'vuetify/es5/util/colors'
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - jfill-store',
-    title: 'jfill-store',
+    titleTemplate: '%s - bae-store',
+    title: 'bae-store',
     htmlAttrs: {
       lang: 'en'
     },
