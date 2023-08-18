682c200ca50b68968239ce146907a528b5b8d804
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2023-08-18 17:30:06 +0700

    printing problem

--- .nuxt/dist/client/5a0e2e5.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[52],{631:function(n,t,e){"use strict";e(29),e(75);t.a={bind:function(n,t,e){n.addEventListener("input",(function(n){var t=n.target.value.replace(/[^0-9]/g,"");"NaN"===(t=parseInt(t,10).toLocaleString("en-US"))&&(t="0"),e.componentInstance.$emit("input",t)}))}}}}]);
