d611d3fde24a52bcda2092a6b4e9f5df4e732a88
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2024-02-11 14:05:58 +0700

    migration v6

--- common/index.js
+++ common/index.js
@@ -387,9 +387,9 @@ export const hostName = () => {
   // const baseURL = 'http://150.95.31.23:8007' // ***DEMO*** 
   // const baseURL = 'http://150.95.31.23:8005' // ***PRODUCTION OBEE*** 
   // const baseURL = 'http://150.95.31.23:8009' // ***PRODUCTION JNONG*** 
-  // const baseURL = 'http://150.95.31.23:8010' // ***PRODUCTION PEEAIR2024*** 
+  const baseURL = 'http://150.95.31.23:8010' // ***PRODUCTION PEEAIR2024*** 
   // const baseURL = 'http://150.95.31.23:8011' // ***PRODUCTION Little boutique2024*** 
-  const baseURL = 'http://150.95.31.23:8101' // ***PRODUCTION Woody3*** 
+  // const baseURL = 'http://150.95.31.23:8101' // ***PRODUCTION Woody3*** 
   // const baseURL = 'http://150.95.31.23:8004' // ***ONLINE UAT*** 
   // return baseURL;
   // let baseURL = `http://150.95.31.23:${process.env.API_PORT}`;
@@ -414,15 +414,15 @@ export const hostName = () => {
 export const mainCompanyInfo = () => {
   // Create a new Date object with the same year and month as the input date, but with day set to 1
   const info = {
-    // name: 'PEEAIR4',
-    // tel: '02091993749',
+    name: 'PEEAIR4',
+    tel: '02091993749',
     // name: 'CHITHANH',
     // tel: '02023378899',
 
     // name: 'Little boutique',
     // tel: '020 58715651',
-    name: 'Woody3',
-    tel: '020 29809752',
+    // name: 'Woody3',
+    // tel: '020 29809752',
     imageUrl: '',
     imageName: '',
     env: 'LIVE'
