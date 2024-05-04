7744b2097c60e4d9740b7f6444e78d4986f2a55c
kittiphansoubin@gmail.com <kittiphanhsoubin@gmail.com>
2024-05-04 12:26:15 +0700

    migration v6

--- common/index.js
+++ common/index.js
@@ -390,8 +390,8 @@ export const hostName = () => {
   // const baseURL = 'http://150.95.31.23:8010' // ***PRODUCTION PEEAIR2024*** 
   // const baseURL = 'http://150.95.31.23:8011' // ***PRODUCTION Little boutique2024*** 
   // const baseURL = 'http://150.95.31.23:8013' // ***PRODUCTION Tonoo*** 
-  // const baseURL = 'http://150.95.31.23:8101' // ***PRODUCTION Woody*** 
-  const baseURL = 'http://150.95.31.23:8004' // ***ONLINE UAT*** 
+  const baseURL = 'http://150.95.31.23:8101' // ***PRODUCTION Woody*** 
+  // const baseURL = 'http://150.95.31.23:8004' // ***ONLINE UAT*** 
   // return baseURL;
   // let baseURL = `http://150.95.31.23:${process.env.API_PORT}`;
 
